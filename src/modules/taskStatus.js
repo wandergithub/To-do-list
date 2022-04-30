5db7bdaff494b9ebb96b7f095e4580e94e920d1b
wandergithub <wandergonzalez0@hotmail.com>
2022-04-30 16:27:04 -0400

    Check tasks text when box chenck

--- src/modules/taskStatus.js
+++ src/modules/taskStatus.js
@@ -1,12 +1,17 @@
 function taskStatus(event) {
   let list = JSON.parse(localStorage.getItem('Tasks'));
   let taskChecked = Number(event.currentTarget.parentNode.parentNode.classList[0].slice(5,));
-  console.log(event.currentTarget.parentNode.parentNode.classList[0].slice(5,));
+  // console.log(list[taskChecked].completed);
+  if (!list[taskChecked].completed) {
+    event.currentTarget.parentNode.parentNode.firstChild.lastChild.style.textDecoration = "line-through";
+  }else {
+    event.currentTarget.parentNode.parentNode.firstChild.lastChild.style.textDecoration = "none";
+  }
   list.forEach(element => {
     if (event.currentTarget.nodeType === 1) {
       if (element.index === taskChecked) {
         element.completed = !element.completed;
-        console.log(element.completed);
+        // console.log(element.completed);
       }
     }
   });
