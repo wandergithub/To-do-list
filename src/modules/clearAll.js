44e06f9f226758d038899bd8a01616ada3fe0318
wandergithub <wandergonzalez0@hotmail.com>
2022-04-30 17:09:02 -0400

    linters

--- src/modules/clearAll.js
+++ src/modules/clearAll.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('button-container');
 const butonHandler = button.addEventListener('click', () => {
-  let list = JSON.parse(localStorage.getItem('Tasks'));
-  const result = list.filter(element => element.completed !== true);
+  const list = JSON.parse(localStorage.getItem('Tasks'));
+  const result = list.filter((element) => element.completed !== true);
   localStorage.setItem('Tasks', JSON.stringify(result));
 });
 
