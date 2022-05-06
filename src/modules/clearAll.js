a50cac1dfe334e02c4a63a020b7bf56a3bd1ecc3
wandergithub <wandergonzalez0@hotmail.com>
2022-05-05 22:20:02 -0400

    Solve linter errors

--- src/modules/clearAll.js
+++ src/modules/clearAll.js
@@ -1,15 +1,15 @@
-const button = document.getElementById('button-container');
 function removeCompletedTasks() {
   const list = JSON.parse(localStorage.getItem('Tasks'));
   const result = list.filter((element) => element.completed !== true);
   localStorage.setItem('Tasks', JSON.stringify(result));
 }
 
-function butonHandler(button) {
+function butonHandler() {
+  const button = document.getElementById('button-container');
   button.addEventListener('click', () => {
     removeCompletedTasks();
   });
 }
 
-export {removeCompletedTasks};
+export { removeCompletedTasks };
 export default butonHandler;
